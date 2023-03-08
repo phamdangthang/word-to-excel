c33a7300281c9f722bfbbb0fe9d44dbac1cd0e7c
ThangPham <thangpd@yopaz.vn>
2023-03-08 13:59:59 +0700

    retest deploy

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const wordPath = __dirname+`/data/`
 const excelPath = __dirname+`/export/`
 
 fs.readdirSync(wordPath).forEach(wordFile => {
-  console.log('Converting: ', wordFile);
+  console.log('Converting: ', wordFile)
   handle(wordFile)
 }); 
 
